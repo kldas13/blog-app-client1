e9fa8fa772806f7a795d6c812de23982597cc08a
K L Das <119942781+kldas13@users.noreply.github.com>
2022-12-28 22:12:41 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -41,11 +41,11 @@ function Signup() {
     return (
         <div>
             <div>Signup</div>
-            <div className="login_cotainer">
-                <div className="input_fields">
-                    <input type="text" name="Email" id="Email" placeholder='Email' onChange={e => setEmail(e.target.value)} />
-                    <input type="password" name="password" id="password" placeholder='password' onChange={e => setPassword(e.target.value)} />
-                    <input type="password" name="confirmpassword" id="confirmpassword" placeholder='confirmpassword' onChange={e => setConfirmpassword(e.target.value)} />
+            <div className="login_cotainer" style={{backgroundColor: "lightblue", height:'170px', width: '200px', marginLeft: '43%', marginTop:'5%', borderRadius:'5px'}}>
+                <div className="input_fields" style={{paddingTop:'10px'}}>
+                    <input type="text" name="Email" id="Email" placeholder='Email' onChange={e => setEmail(e.target.value)} style={{margin:'10px'}} />
+                    <input type="password" name="password" id="password" placeholder='password' onChange={e => setPassword(e.target.value)} style={{margin:'10px'}}/>
+                    <input type="password" name="confirmpassword" id="confirmpassword" placeholder='confirmpassword' onChange={e => setConfirmpassword(e.target.value)} style={{margin:'10px'}} />
                     <button className="signup" onClick={handleSignup}>signup</button>
 
                 </div>
